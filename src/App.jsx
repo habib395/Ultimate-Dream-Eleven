bc1963cdc10e9ecfdc666268f4172127f2e084be
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-10-25 01:16:12 +0600

    navbar section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import Available from './components/Available'
+import Banner from './components/Banner'
+import Header from './components/Header'
 import './index.css'
 
 function App() {
 
   return (
-    <>
+    <div className='container mx-auto'>
       
-      <h3>Vite + React</h3>
-      <button className="btn btn-warning"> Button is coming</button>
-     
-      
-    </>
+      {/* header section */}
+     <Header></Header>
+      {/* banner section */}
+      <Banner></Banner>
+      {/* available section */}
+      <Available></Available>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='container mx-auto'>
+        <div className='flex items-center justify-between py-10'>
+            <div><img style={{width: '50px'}} src="./../../assets/logo.png" alt="" /></div>
+            <div className='flex gap-10'>
+                <p>Home</p>
+                <p>Fixture</p>
+                <p>Teams</p>
+                <p>Schedules</p>
+                <button className='font-bold flex items-center border  px-3 rounded-lg bg-transparent gap-1'><span>0</span> Coin
+                <img style={{width: '15px'}} src="./../../assets/coin.png" alt="" />
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
